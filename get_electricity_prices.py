6914ddbc5b3fc257ada9f26e503dd6f4917c62fe
Oskar Alfredsson <oskaralf@kth.se>
2024-12-25 11:37:01 +0100

    test check

--- get_electricity_prices.py
+++ get_electricity_prices.py
@@ -72,8 +72,10 @@ def get_todays_data(price_class):
 
 
 def main():
-    for price_class in ["SE1", "SE2", "SE3", "SE4"]:
-        get_data(price_class)
+    #for price_class in ["SE1", "SE2", "SE3", "SE4"]:
+        #get_data(price_class)
+    df = get_todays_data('SE4')
+    print(df)
 
 if __name__ == "__main__":
     main()
